077de869ca8e7bdad8faa0c51a3ecf7f37db2f2c
Tinkachen <catharina.herchert@trevisto.de>
2017-10-16 20:39:02 +0200

    updated data model and removed unused classes/files

--- funtours/Model/ModelManager.swift
+++ funtours/Model/ModelManager.swift
@@ -12,7 +12,7 @@ import SwiftyJSON
 
 class ModelManager: NSObject {
     
-    static let sharedInstance = ModelManager.init()
+    static let sharedInstance = ModelManager()
     
     var tourEntries: [TourData] = []
     
@@ -26,5 +26,139 @@ class ModelManager: NSObject {
         
     }
     
+    
+    /////////////
+    static func createPopularDummyData () -> [TourData] {
+        var data: [TourData] = [TourData]()
+        
+        data.append(TourData(
+            identifier: "1234",
+            tourName: "Along the wall",
+            tourDescription: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis.",
+            creator: "T. Mendez",
+            creationTime: "01/2017",
+            image: UIImage(named: "Image-3")!,
+            likes: 10,
+            favs: 5,
+            comments: [Comment(creator: "P.Pan", date: "02/2017", text: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.", likes: 20),
+                       Comment(creator: "P.Pan", date: "02/2017", text: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.", likes: 20)],
+            stations: [Station(identifier: "1234", stationName: "First Things First", kind: "Art", adress: "Mustermannallee 200, 12345 Berlin", number: 1, informations: "Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.", likes: 2, website: URL(string: ""), yelp: URL(string: "")),
+                       Station(identifier: "1234", stationName: "First Things First", kind: "Art", adress: "Mustermannallee 200, 12345 Berlin", number: 1, informations: "Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.", likes: 2, website: URL(string: ""), yelp: URL(string: ""))],
+            rdmColor: UIColor.randomColor()))
+        
+        data.append(TourData(
+            identifier: "1234",
+            tourName: "Multiculti",
+            tourDescription: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis.",
+            creator: "T. Mendez",
+            creationTime: "01/2017",
+            image: UIImage(named: "Image-1")!,
+            likes: 10,
+            favs: 5,
+            comments: [Comment(creator: "P.Pan", date: "02/2017", text: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.", likes: 20),
+                       Comment(creator: "P.Pan", date: "02/2017", text: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.", likes: 20)],
+            stations: [Station(identifier: "1234", stationName: "First Things First", kind: "Art", adress: "Mustermannallee 200, 12345 Berlin", number: 1, informations: "Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.", likes: 2, website: URL(string: ""), yelp: URL(string: "")),
+                       Station(identifier: "1234", stationName: "First Things First", kind: "Art", adress: "Mustermannallee 200, 12345 Berlin", number: 1, informations: "Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.", likes: 2, website: URL(string: ""), yelp: URL(string: ""))],
+            rdmColor: UIColor.randomColor()))
+                
+        
+        return data
+    }
+    
+    static func createDummyData () -> [TourData] {
+        var data: [TourData] = [TourData]()
+        
+        data.append(TourData(
+            identifier: "1234",
+            tourName: "Along the wall",
+            tourDescription: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis.",
+            creator: "T. Mendez",
+            creationTime: "01/2017",
+            image: UIImage(named: "Image-3")!,
+            likes: 10,
+            favs: 5,
+            comments: [Comment(creator: "P.Pan", date: "02/2017", text: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.", likes: 20),
+                       Comment(creator: "P.Pan", date: "02/2017", text: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.", likes: 20)],
+            stations: [Station(identifier: "1234", stationName: "First Things First", kind: "Art", adress: "Mustermannallee 200, 12345 Berlin", number: 1, informations: "Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.", likes: 2, website: URL(string: ""), yelp: URL(string: "")),
+                       Station(identifier: "1234", stationName: "First Things First", kind: "Art", adress: "Mustermannallee 200, 12345 Berlin", number: 1, informations: "Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.", likes: 2, website: URL(string: ""), yelp: URL(string: ""))],
+            rdmColor: UIColor.randomColor()))
+        
+        data.append(TourData(
+            identifier: "1234",
+            tourName: "Multiculti",
+            tourDescription: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis.",
+            creator: "T. Mendez",
+            creationTime: "01/2017",
+            image: UIImage(named: "Image-1")!,
+            likes: 10,
+            favs: 5,
+            comments: [Comment(creator: "P.Pan", date: "02/2017", text: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.", likes: 20),
+                       Comment(creator: "P.Pan", date: "02/2017", text: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.", likes: 20)],
+            stations: [Station(identifier: "1234", stationName: "First Things First", kind: "Art", adress: "Mustermannallee 200, 12345 Berlin", number: 1, informations: "Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.", likes: 2, website: URL(string: ""), yelp: URL(string: "")),
+                       Station(identifier: "1234", stationName: "First Things First", kind: "Art", adress: "Mustermannallee 200, 12345 Berlin", number: 1, informations: "Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.", likes: 2, website: URL(string: ""), yelp: URL(string: ""))],
+            rdmColor: UIColor.randomColor()))
+        
+        data.append(TourData(
+            identifier: "1234",
+            tourName: "Along the wall",
+            tourDescription: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis.",
+            creator: "T. Mendez",
+            creationTime: "01/2017",
+            image: UIImage(named: "Image-2")!,
+            likes: 10,
+            favs: 5,
+            comments: [Comment(creator: "P.Pan", date: "02/2017", text: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.", likes: 20),
+                       Comment(creator: "P.Pan", date: "02/2017", text: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.", likes: 20)],
+            stations: [Station(identifier: "1234", stationName: "First Things First", kind: "Art", adress: "Mustermannallee 200, 12345 Berlin", number: 1, informations: "Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.", likes: 2, website: URL(string: ""), yelp: URL(string: "")),
+                       Station(identifier: "1234", stationName: "First Things First", kind: "Art", adress: "Mustermannallee 200, 12345 Berlin", number: 1, informations: "Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.", likes: 2, website: URL(string: ""), yelp: URL(string: ""))],
+            rdmColor: UIColor.randomColor()))
+        
+        data.append(TourData(
+            identifier: "1234",
+            tourName: "Multiculti",
+            tourDescription: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis.",
+            creator: "T. Mendez",
+            creationTime: "01/2017",
+            image: UIImage(named: "Image-2")!,
+            likes: 10,
+            favs: 5,
+            comments: [Comment(creator: "P.Pan", date: "02/2017", text: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.", likes: 20),
+                       Comment(creator: "P.Pan", date: "02/2017", text: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.", likes: 20)],
+            stations: [Station(identifier: "1234", stationName: "First Things First", kind: "Art", adress: "Mustermannallee 200, 12345 Berlin", number: 1, informations: "Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.", likes: 2, website: URL(string: ""), yelp: URL(string: "")),
+                       Station(identifier: "1234", stationName: "First Things First", kind: "Art", adress: "Mustermannallee 200, 12345 Berlin", number: 1, informations: "Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.", likes: 2, website: URL(string: ""), yelp: URL(string: ""))],
+            rdmColor: UIColor.randomColor()))
+        
+        data.append(TourData(
+            identifier: "1234",
+            tourName: "Along the wall",
+            tourDescription: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis.",
+            creator: "T. Mendez",
+            creationTime: "01/2017",
+            image: UIImage(named: "Image-3")!,
+            likes: 10,
+            favs: 5,
+            comments: [Comment(creator: "P.Pan", date: "02/2017", text: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.", likes: 20),
+                       Comment(creator: "P.Pan", date: "02/2017", text: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.", likes: 20)],
+            stations: [Station(identifier: "1234", stationName: "First Things First", kind: "Art", adress: "Mustermannallee 200, 12345 Berlin", number: 1, informations: "Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.", likes: 2, website: URL(string: ""), yelp: URL(string: "")),
+                       Station(identifier: "1234", stationName: "First Things First", kind: "Art", adress: "Mustermannallee 200, 12345 Berlin", number: 1, informations: "Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.", likes: 2, website: URL(string: ""), yelp: URL(string: ""))],
+            rdmColor: UIColor.randomColor()))
+        
+        data.append(TourData(
+            identifier: "1234",
+            tourName: "Multiculti",
+            tourDescription: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis.",
+            creator: "T. Mendez",
+            creationTime: "01/2017",
+            image: UIImage(named: "Image-1")!,
+            likes: 10,
+            favs: 5,
+            comments: [Comment(creator: "P.Pan", date: "02/2017", text: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.", likes: 20),
+                       Comment(creator: "P.Pan", date: "02/2017", text: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.", likes: 20)],
+            stations: [Station(identifier: "1234", stationName: "First Things First", kind: "Art", adress: "Mustermannallee 200, 12345 Berlin", number: 1, informations: "Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.", likes: 2, website: URL(string: ""), yelp: URL(string: "")),
+                       Station(identifier: "1234", stationName: "First Things First", kind: "Art", adress: "Mustermannallee 200, 12345 Berlin", number: 1, informations: "Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.", likes: 2, website: URL(string: ""), yelp: URL(string: ""))],
+            rdmColor: UIColor.randomColor()))
+        
+        return data
+    }
     
 }
--- funtours/Model/TourDataModel.swift
+++ funtours/Model/TourDataModel.swift
@@ -9,76 +9,41 @@
 import Foundation
 import UIKit
 
-class TourData: Equatable {
+struct Comment {
+    let creator: String
+    let date: String
+    let text: String
     
-    var identifier: String?
-    var tourName: String?
-    var tourDescription: String?
-    var creator: String?
-    var creationTime: String?
-    var image: UIImage?
-    
-    var likes: Int?
-    var favs: Int?
-    var comments: [Comments]?
-    
-    var stations: [Stations]?
-    
-    var rdmColor: UIColor?
-    
-    init(identifier: String?, tourName: String?, tourDescription: String?, creator: String?, creationTime: String?, image: UIImage?, likes: Int?, favs: Int?, comments: [Comments]?, stations: [Stations]?, rdmColor: UIColor?) {
-        self.identifier = identifier
-        self.tourName = tourName
-        self.tourDescription = tourDescription
-        self.creator = creator
-        self.creationTime = creationTime
-        self.image = image
-        self.likes = likes
-        self.favs = favs
-        self.comments = comments
-        self.stations = stations
-        self.rdmColor = rdmColor
-    }
-    
-    func createTourEntry () -> TourData {
-        
-        return TourData(identifier: identifier, tourName: tourName, tourDescription: tourDescription, creator: creator, creationTime: creationTime, image: image, likes: likes, favs: favs, comments: comments, stations: stations, rdmColor: rdmColor)
-    }
-    
-    static func createPopularDummyData () -> [TourData] {
-        var data: [TourData] = [TourData]()
-        
-        data.append(TourData(identifier: "1234", tourName: "Along the wall", tourDescription: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis.", creator: "T. Mendez", creationTime: "01/2017", image: UIImage(named: "Image-3"), likes: 10, favs: 5,
-                             comments: [Comments(creator: "P.Pan", date: "01/2017", text: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.", likes: 2)],
-                             stations: [Stations(identifier: "A1B2", stationName: "First things first", kind: "Art", adress: "Mustermannstraße 10, 10117 Berlin", number: 1, informations: "Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.", likes: 0, website: URL(string: ""), yelp: URL(string: ""))],
-                             rdmColor: UIColor.Colors.randomColor()))
-        
-        data.append(TourData(identifier: "1234", tourName: "Berlin Art", tourDescription: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis.", creator: "T. Mendez", creationTime: "01/2017", image: UIImage(named: "Image-1"), likes: 10, favs: 5,
-                             comments: [Comments(creator: "P.Pan", date: "01/2017", text: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.", likes: 2)],
-                             stations: [Stations(identifier: "A1B2", stationName: "First things first", kind: "Art", adress: "Mustermannstraße 10, 10117 Berlin", number: 1, informations: "Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.", likes: 0, website: URL(string: ""), yelp: URL(string: ""))],
-                             rdmColor: UIColor.Colors.randomColor()))
-        
-        return data
-    }
-    
-    static func createDummyData () -> [TourData] {
-        var data: [TourData] = [TourData]()
-        
-        data.append(TourData(identifier: "1234", tourName: "Along the wall", tourDescription: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis.", creator: "T. Mendez", creationTime: "01/2017", image: UIImage(named: "Image-3"), likes: 10, favs: 5,
-                             comments: [Comments(creator: "P.Pan", date: "01/2017", text: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.", likes: 2)],
-                             stations: [Stations(identifier: "A1B2", stationName: "First things first", kind: "Art", adress: "Mustermannstraße 10, 10117 Berlin", number: 1, informations: "Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.", likes: 0, website: URL(string: ""), yelp: URL(string: ""))],
-                             rdmColor: UIColor.Colors.randomColor()))
-        
-        data.append(TourData(identifier: "1234", tourName: "Berlin Art", tourDescription: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis.", creator: "T. Mendez", creationTime: "01/2017", image: UIImage(named: "Image-1"), likes: 10, favs: 5,
-                             comments: [Comments(creator: "P.Pan", date: "01/2017", text: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.", likes: 2)],
-                             stations: [Stations(identifier: "A1B2", stationName: "First things first", kind: "Art", adress: "Mustermannstraße 10, 10117 Berlin", number: 1, informations: "Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.", likes: 0, website: URL(string: ""), yelp: URL(string: ""))],
-                             rdmColor: UIColor.Colors.randomColor()))
-        
-        return data
-    }
-    
+    let likes: Int
 }
 
-func == (lhs: TourData, rhs: TourData) -> Bool {
-    return lhs.identifier == rhs.identifier
+struct Station {
+    let identifier: String
+    let stationName: String
+    let kind: String
+    let adress: String
+    let number: Int
+    
+    let informations: String
+    
+    let likes: Int
+    let website: URL?
+    let yelp: URL?
+}
+
+struct TourData {
+    let identifier: String
+    let tourName: String
+    let tourDescription: String
+    let creator: String
+    let creationTime: String
+    let image: UIImage
+    
+    let likes: Int
+    let favs: Int
+    let comments: [Comment]?
+    
+    let stations: [Station]?
+    
+    let rdmColor: UIColor?
 }
